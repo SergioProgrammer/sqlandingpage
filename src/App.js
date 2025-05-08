73ac0630b5fbd11bd95bbf61b97bbeecfc370350
Sergio <sergiosanpacheco@gmail.com>
2025-05-08 15:53:17 +0100

    commit.loading

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, { useEffect } from 'react';
-import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
+import React, { useEffect, useState } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Brands from './components/Brands.js';
 import Hero from './components/Hero.js';
 import Services from './components/Services.js';
 import Sliderletras from './components/Sliderletras.js';
 import TimelineV2 from './components/TimelineV2.js';
 import Cta from './components/Cta.js';
-import './styles/index.css'; 
 import Footer from './components/Footer.js';
+import './styles/index.css';
+import './styles/Loader.css'; // Archivo CSS para la barra de carga
 
 function App() {
-  useEffect(() => {
+  const [isLoading, setIsLoading] = useState(true);
 
-    const timer = setTimeout(() => {
-      console.log("App.js: Attempting to sort and refresh ScrollTriggers...");
+  useEffect(() => {
+    // Detecta cuando la página ha cargado completamente
+    const handleLoad = () => {
+      setIsLoading(false); // Oculta la barra de carga
       ScrollTrigger.sort();
       ScrollTrigger.refresh();
-
-      console.log("App.js: ScrollTriggers sorted and refreshed.");
-    }, 200); 
-    return () => {
-      clearTimeout(timer);
-      console.log("App.js: Cleaned up timeout for ScrollTrigger refresh.");
     };
-  }, []); 
+
+    // Escucha el evento de carga completa
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad); // Limpia el evento al desmontar
+    };
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loader">
+        <div className="loader-text">Loading...</div>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
